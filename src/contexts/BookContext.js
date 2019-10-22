0a975fce9d26d82565837ffdb1998b5dd32786f6
davidgeorgebell <bell.dave93@gmail.com>
2019-10-22 15:59:36 +0800

    Books are now added to local storage using useEffect hook

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,20 +1,28 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 import uuid from 'uuid/v1'
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-       {title: 'meditations',  author: 'marcus aurelius', id: 1},
-       {title: 'letters from a stoic', author: 'seneca the younger', id: 2} 
-    ]);
+    const [books, setBooks] = useState([]);
     const addBook = (title, author) => {
         setBooks([...books, { title, author, id: uuid() }])
     }
     const removeBook = (id) => {
         setBooks(books.filter(book => book.id !== id))
     }
+    useEffect(() => {
+        const data = localStorage.getItem('books');
+        if (data) {
+            setBooks(JSON.parse(data));
+        }
+    }, []);
+    
+    useEffect(() => {
+       localStorage.setItem('books', JSON.stringify(books))
+    }, [books]);
+
     return(
         <BookContext.Provider value={{books, addBook, removeBook}}>
             { props.children }
